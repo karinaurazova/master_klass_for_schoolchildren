f7b86b6abaa25a1680899759e83b5080ada86ea0
Karina Urazova <karinaurazowa2@yandex.ru>
2025-07-11 21:06:22 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import os
 from voila.app import main
 
-port = int(os.environ.get('PORT', 8866))
-main(["--port=%d" % port, "--no-browser", "--Voila.ip=0.0.0.0", "master_klass_mathmodelling_in_medicine_for_schoolchildren.ipynb"])
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8866))
+    main([
+        "--port=%d" % port,
+        "--no-browser",
+        "--Voila.ip=0.0.0.0",
+        "--enable_nbextensions=True",
+        "master_klass_mathmodelling_in_medicine_for_schoolchildren.ipynb"
+    ])
